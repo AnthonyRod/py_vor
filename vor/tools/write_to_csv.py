acbf90b1b274ca7c679edd6f225e131bb9652a6f
Rodrigo <rodrigocf_92@hotmail.com>
2018-07-10 19:18:46 -0600

    added docstring

--- vor/tools/write_to_csv.py
+++ vor/tools/write_to_csv.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""Writes a nested list to a CSV file"""
+"""write_to_csv
+Writes a nested list to a CSV file.
+Author: ksco92
+"""
 
 import csv
 
@@ -11,6 +14,8 @@ import csv
 
 
 def write_to_csv(destination_file, content, delim=','):
+
+    """Writes a nested list to a CSV file."""
 
     with open(destination_file, 'w', encoding='UTF-8', newline='') as f:
         writer = csv.writer(f, delimiter=delim)
