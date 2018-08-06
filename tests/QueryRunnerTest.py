2d5bc5a4dea3bb62223a330691d2b6af1448b83e
Rodrigo <rodrigocf_92@hotmail.com>
2018-08-06 10:10:34 -0600

    Added new test.

--- tests/QueryRunnerTest.py
+++ tests/QueryRunnerTest.py
@@ -6,7 +6,7 @@ from py_vor.tools.get_secret import get_secret
 
 class QueryRunnerTest(unittest.TestCase):
 
-    def test_mysql_select_query(self):
+    def test_mysql_select_nested_list(self):
         secret = get_secret('ksco92-MySQL-Secret')
 
         runner = QueryRunner(secret['engine'], secret['host'], secret['port'], secret['username'], secret['password'],
@@ -15,6 +15,21 @@ class QueryRunnerTest(unittest.TestCase):
         results = runner.execute_all()
 
         expected = [['10001', '1953-09-02', 'Georgi', 'Facello', 'M', '1986-06-26']]
+
+        assert results == expected
+
+    def test_mysql_select_dict(self):
+        secret = get_secret('ksco92-MySQL-Secret')
+
+        runner = QueryRunner(secret['engine'], secret['host'], secret['port'], secret['username'], secret['password'],
+                             secret['dbname'], 'queries/select_user.sql', returns_rows=True, include_headers=False,
+                             return_as='dict')
+
+        results = runner.execute_all()
+
+        expected = {'data': [
+            {'emp_no': '10001', 'birth_date': '1953-09-02', 'first_name': 'Georgi', 'last_name': 'Facello',
+             'gender': 'M', 'hire_date': '1986-06-26'}]}
 
         assert results == expected
 
